f72114325b93ab6c5476b4815e75cacbd5af873d
Stanley Masinde <stanmasinde@gmail.com>
2023-02-01 17:09:47 +0300

    new schema for users  + add bio and timestamps

---
+++ database/migrations/20230201140142_create_users_table.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+	return knex.schema.createTable('users', (table) => {
+		table.increments('id')
+		table.string('name')
+		table.string('email')
+		table.string('password')
+		table.text('bio')
+		table.timestamps(true, true)
+	})
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+	return knex.schema.dropTable('users')
+}
