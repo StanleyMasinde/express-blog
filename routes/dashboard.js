6a983e6386427c9beefa643f2efa2a1d661eb65f
Stanley Masinde <stanmasinde@gmail.com>
2023-02-01 15:48:11 +0300

    placeholder page for the posts

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,11 +1,26 @@
 const router = require('express').Router()
 const { queryBuilder } = require('../app/models/model')
 
-router.get('/', async(req, res) => {
-	const postCount = await queryBuilder('posts').count('* as agg')
-	res.render('dashboard', {
-		postCount: postCount[0].agg
-	})
+router.get('/', async (req, res, next) => {
+	try {
+		const postCount = await queryBuilder('posts').count('* as agg')
+		res.render('dashboard', {
+			postCount: postCount[0].agg
+		})
+	} catch (error) {
+		next(error)
+	}
+})
+
+router.get('/posts', async (req, res, next) => {
+	try {
+		const posts = queryBuilder('posts').select()
+		res.render('posts', {
+			posts
+		})
+	} catch (error) {
+		next(error)
+	}
 })
 
 module.exports = router
