930e070429d88ea944978b12991748eb62735cc7
Stanley Masinde <stanmasinde@gmail.com>
2023-02-01 17:16:19 +0300

    add index to users email

--- database/migrations/20230201140142_create_users_table.js
+++ database/migrations/20230201140142_create_users_table.js
@@ -6,7 +6,7 @@ exports.up = function (knex) {
 	return knex.schema.createTable('users', (table) => {
 		table.increments('id')
 		table.string('name')
-		table.string('email')
+		table.string('email').unique().index('USER_EMAIL')
 		table.string('password')
 		table.text('bio')
 		table.timestamps(true, true)
