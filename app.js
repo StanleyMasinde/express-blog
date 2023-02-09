e0fbb74a90c537b528ab262a53e52e76efcdcfd5
Stanley Masinde <stanmasinde@gmail.com>
2023-02-09 12:59:15 +0300

    fix(session): getting the session secret from the environment variables

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(session({
-	secret: 'super-secret-cookie', resave: false, saveUninitialized: true, name: 'blog_session',
+	secret: process.env.SECRET, resave: false, saveUninitialized: true, name: 'blog_session',
 }))
 
 app.use('/', indexRouter)
